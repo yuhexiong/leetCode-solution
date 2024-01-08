97a869250b39f7688cb6abd718a0cc765a95fc08
yuhexiong <yuhexiong871210@gmail.com>
2024-01-08 14:56:51 +0800

    feat: Problem 1 Two Sum typescript

---
+++ typescript/0001_TwoSum.ts
@@ -0,0 +1,17 @@
+// Problem 1: Two Sum
+// https://leetcode.com/problems/two-sum/
+
+function twoSum(nums: number[], target: number): number[] {
+    // 使用迴圈單層迴圈
+    for (let i = 0; i < nums.length; i++) {
+        // 如果 希望的值(目標減去當前數值) 在這個array中 且 不是同一個位置的值(index不相同)
+        // 就視為找到答案, 直接return
+        const complement = target - nums[i];
+        const complementIndex = nums.indexOf(complement);
+
+        if (complementIndex !== -1 && i !== complementIndex) {
+            return [i, complementIndex];
+        }
+    }
+    return [];
+}
