b991892749a83ebb181a9a1a7cc9efaeb5056fb8
yuhexiong <yuhexiong871210@gmail.com>
2024-02-16 10:18:59 +0800

    feat: Problem 970 Powerful Integers python

---
+++ python/0970_PowerfulIntegers.py
@@ -0,0 +1,13 @@
+# Problem 970: Powerful Integers
+# https://leetcode.com/problems/powerful-integers/
+
+from typing import List
+
+class Solution:
+    def powerfulIntegers(self, x: int, y: int, bound: int) -> List[int]:
+        # 列出所有小於等於 bound 的 x的次方, y的次方為兩個list
+        xPowers = {x**p for p in range(20) if x**p <= bound}
+        yPowers = {y**p for p in range(20) if y**p <= bound}
+        # 使用雙層迴圈取的兩種數值相加, 且要小於等於 bound
+        ans = list({xp + yp for xp in xPowers for yp in yPowers if xp+yp <= bound})
+        return ans
