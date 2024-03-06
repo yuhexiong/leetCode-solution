65bc57f84547c16f3081a7b3534c3d6e583740fc
yuhexiong <yuhexiong871210@gmail.com>
2024-03-06 14:40:22 +0800

    feat: Problem 565 Array Nesting c++

--- go/0565_ArrayNesting.go
+++ go/0565_ArrayNesting.go
@@ -13,18 +13,18 @@ func arrayNesting(nums []int) int {
 	for i := 0; i < len(nums); i++ {
 		// idx 現在要走的位置, 預設現在的長度為 0
 		idx := i
-		tepLen := 0
+		tempLen := 0
 
-		// 紀錄這點為走過, tepLen + 1, 更新 idx 為指引的下一步位置, 一直走值到該位置已走過
+		// 紀錄這點為走過, tempLen + 1, 更新 idx 為指引的下一步位置, 一直走值到該位置已走過
 		for !visit[idx] {
 			visit[idx] = true
-			tepLen++
+			tempLen++
 			idx = nums[idx]
 		}
 
 		// 如果 現在的長度 大於 最大長度, 則更新最大長度
-		if tepLen > longestLen {
-			longestLen = tepLen
+		if tempLen > longestLen {
+			longestLen = tempLen
 		}
 	}
 	return longestLen
