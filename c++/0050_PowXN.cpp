8f36245dba0911f796d217db1b1c28f69f86f05d
yuhexiong <yuhexiong871210@gmail.com>
2024-03-16 19:07:53 +0800

    feat: Problem 50 Pow(x, n) c++

---
+++ c++/0050_PowXN.cpp
@@ -0,0 +1,30 @@
+// Problem 50: Pow(x, n)
+// https://leetcode.com/problems/powx-n/description/
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        // 預設答案為 1, 宣告一個取絕對值的 n 為 positiveN
+        double ans = 1;
+        bool positive = n >= 0;
+        long long positiveN = (positive) ? n : -static_cast<long long>(n);
+
+        // 只要 positiveN 這個次方還沒有到 0, 就持續跑迴圈
+        while (positiveN > 0) {
+            // 如果這個次方式偶數, 就直接把底數平方, 次方除以2, 優化速度
+            if (positiveN % 2 == 0) {
+                x *= x;
+                positiveN /= 2;
+            }
+            // 正常乘一次, 次方 - 1
+            ans *= x;
+            positiveN--;
+        }
+        
+        // 如果原先 n 是正數就回傳, 負數就回傳導數
+        return (positive) ? ans : 1 / ans;
+    }
+};
