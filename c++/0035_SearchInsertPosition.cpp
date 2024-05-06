b3111f93d675ead2959e5680790b1a1236963a63
yuhexiong <yuhexiong871210@gmail.com>
2024-05-06 14:52:32 +0800

    feat: Problem 35 Search Insert Position C

--- c++/0035_SearchInsertPosition.cpp
+++ c++/0035_SearchInsertPosition.cpp
@@ -8,17 +8,18 @@ class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
         // 先預設答案在 index = 0 的位置
-        int output=0;
+        int output = 0;
 
-        // 跑迴圈, 如果目前的值小於目標 就認定目標要在他之後, ans往上加 繼續往後比對
+        // 跑迴圈, 如果目前的值小於目標 就認定目標要在他之後, output 往上加 繼續往後比對
         // 如果 >= 就代表可以加入, 跳脫迴圈
-        // 如果跑完 皆小於, ans也是最後的位置
-        for (int i=0; i<nums.size(); i++){
-            if (nums[i]<target){
+        // 如果跑完 皆小於, output 也是最後的位置
+        for (int i = 0; i < nums.size(); i++){
+            if (nums[i] < target){
                 output++;
             }
             else break;
         }
+
         return output;
     }
 };
---
+++ c/0035_SearchInsertPosition.c
@@ -0,0 +1,18 @@
+// Problem 35: Search Insert Position
+// https://leetcode.com/problems/search-insert-position/
+
+int searchInsert(int* nums, int numsSize, int target) {
+    int output = 0;
+
+    // 跑迴圈, 如果目前的值小於目標 就認定目標要在他之後, output 往上加 繼續往後比對
+    // 如果 >= 就代表可以加入, 跳脫迴圈
+    // 如果跑完 皆小於, output 也是最後的位置
+    for (int i = 0; i < numsSize; i++){
+        if (nums[i] < target){
+            output++;
+        }
+        else break;
+    }
+    
+    return output;
+}
