aab24d89bac1dbe1cb12afb7d40b85b967186a3d
yuhexiong <yuhexiong871210@gmail.com>
2024-05-14 17:17:22 +0800

    feat: Problem 2723 Add Two Promises JavaScript

---
+++ javascript/2723_AddTwoPromises.js
@@ -0,0 +1,8 @@
+// Problem 2723: Add Two Promises
+// https://leetcode.com/problems/add-two-promises/
+
+var addTwoPromises = async function(promise1, promise2) {
+  // 使用 Promise.all 讓兩個 Promise 同步處理, 並 await 兩者的結果再相加
+  const [num1, num2] = await Promise.all([promise1, promise2]);
+  return num1 + num2
+};
