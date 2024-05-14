39c3e5b2e2e6ca77075d4c7676bf5fd6c107f2e9
yuhexiong <yuhexiong871210@gmail.com>
2024-05-14 17:19:36 +0800

    fix: set class prevent same function name of js and ts

--- javascript/2723_AddTwoPromises.js
+++ javascript/2723_AddTwoPromises.js
@@ -1,8 +1,10 @@
 // Problem 2723: Add Two Promises
 // https://leetcode.com/problems/add-two-promises/
 
-var addTwoPromises = async function(promise1, promise2) {
-  // 使用 Promise.all 讓兩個 Promise 同步處理, 並 await 兩者的結果再相加
-  const [num1, num2] = await Promise.all([promise1, promise2]);
-  return num1 + num2
-};
+javaScriptProblemSet = {
+  addTwoPromises: async function(promise1, promise2) {
+    // 使用 Promise.all 讓兩個 Promise 同步處理, 並 await 兩者的結果再相加
+    const [num1, num2] = await Promise.all([promise1, promise2]);
+    return num1 + num2
+  }
+}
